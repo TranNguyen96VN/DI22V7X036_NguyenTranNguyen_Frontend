e399d3aeb9841193705c598d3e9c20cef36867d1
nguyendi22v7x036 <nguyentrannguyen170396@gmail.com>
2024-09-25 16:15:03 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+//Cài đặt router
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+import ContactAdd from "@/views/ContactAdd.vue";
+const routes = [
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+    {
+        path: "/contacts",
+        name: "contact.add",
+        component: ContactAdd
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    }
+    
+];
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
+});
+export default router;
