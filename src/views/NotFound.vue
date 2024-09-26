60a2bc6f30bff9cf5003708c4ac9b1f17ec49418
nguyendi22v7x036 <nguyentrannguyen170396@gmail.com>
2024-09-26 10:30:00 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Oops, Không thể tìm thấy trang yêu cầu. Trở về
+            <router-link to="/">TRANG CHỦ.</router-link>
+        </p>
+    </div>
+</template>
